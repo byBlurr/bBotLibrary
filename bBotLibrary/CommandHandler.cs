d6523d3fc25125df65ef0dfbc664b850e8230f89
byBlurr <dbldumont11@gmail.com>
2020-06-21 15:42:33 +0100

    Add project files.

---
+++ bBotLibrary/CommandHandler.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading.Tasks;
+using System.Reflection;
+using Discord.Commands;
+using Discord.WebSocket;
+using Discord;
+using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Discord.Net.Bot
+{
+    public abstract class CommandHandler
+    {
+        private string prefix;
+        private CommandService commands;
+        private static DiscordSocketClient bot;
+        private IServiceProvider map;
+
+        public CommandHandler(IServiceProvider provider, string _prefix = "!")
+        {
+            prefix = _prefix;
+            map = provider;
+            bot = map.GetService<DiscordSocketClient>();
+            commands = map.GetService<CommandService>();
+
+            bot.MessageReceived += HandleCommandAsync;
+        }
+
+        private async Task HandleCommandAsync(SocketMessage pMsg)
+        {
+            SocketUserMessage message = pMsg as SocketUserMessage;
+            if (message == null) return;
+            var context = new SocketCommandContext(bot, message);
+            if (message.Author.IsBot) return;
+                
+            int argPos = 0;
+            if (message.HasStringPrefix(prefix, ref argPos))
+            {
+                var result = await commands.ExecuteAsync(context, argPos, map);
+                if (!result.IsSuccess && result.ErrorReason != "Unknown command.") Console.WriteLine(result.ErrorReason);
+            }
+        }
+
+        public async Task ConfigureAsync() { await commands.AddModulesAsync(Assembly.GetEntryAssembly(), map); }
+
+        public static DiscordSocketClient GetBot() => bot;
+    }
+}
