9c74cfe6f2eb9d2be16575352b016d6c6d64b376
byBlurr <dbldumont11@gmail.com>
2020-06-21 23:14:27 +0100

    Add moderation command module

--- bModerator/ModeratorBot.cs
+++ bModerator/ModeratorBot.cs
@@ -1,5 +1,7 @@
 ﻿using Discord;
+using Discord.Commands;
 using Discord.Net.Bot;
+using Discord.Net.Bot.CommandModules;
 using Discord.WebSocket;
 using System;
 using System.Threading.Tasks;
@@ -13,7 +15,7 @@ namespace bModerator
             CommandHandler handler = new MCommandHandler();
 
             // Todo: Move token and prefix into a config file
-            StartBot(handler, "NzEzODcwMzIyNjYyNzY4NzI4.Xu_IoQ.ItWLiWYGIiCrOoCIzBpcVdtuKTY", "-");
+            StartBot(handler, "NzEzODcwMzIyNjYyNzY4NzI4.Xu_Pxw.DqT0a3HAfVF4DEy4s-_cBf0qlBY", "~");
         }
     }
 
@@ -22,6 +24,7 @@ namespace bModerator
         public override void SetupHandlers(DiscordSocketClient bot)
         {
             bot.Ready += ReadyAsync;
+            bot.MessageReceived += ModuleModerator.FilterMutedAsync;
         }
 
         private async Task ReadyAsync()
@@ -34,8 +37,32 @@ namespace bModerator
         }
     }
 
-    class MCommandModule : ModuleBase
+    public class MCommandModule : ModuleBase
     {
+        [Command("kick")]
+        [Alias("k")]
+        [RequireUserPermission(GuildPermission.KickMembers)]
+        private async Task KickAsync(IGuildUser user, [Remainder] string reason) => await ModuleModerator.KickAsync(Context, user, reason);
 
+        [Command("ban")]
+        [Alias("b")]
+        [RequireUserPermission(GuildPermission.BanMembers)]
+        private async Task BanAsync(IGuildUser user, [Remainder] string reason) => await ModuleModerator.BanAsync(Context, user, reason);
+
+        [Command("warn")]
+        [Alias("w")]
+        [RequireUserPermission(GuildPermission.KickMembers)]
+        private async Task WarnAsync(IGuildUser user, [Remainder] string reason) => await ModuleModerator.WarnAsync(Context, user, reason);
+
+        [Command("mute")]
+        [Alias("m")]
+        [RequireUserPermission(GuildPermission.KickMembers)]
+        private async Task MuteAsync(IGuildUser user, [Remainder] string reason) => await ModuleModerator.MuteAsync(Context, user, reason);
+
+        [Command("unmute")]
+        [Alias("um")]
+        [RequireUserPermission(GuildPermission.KickMembers)]
+        private async Task UnmuteAsync(IGuildUser user) => await ModuleModerator.UnmuteAsync(Context, user);
+        
     }
 }
