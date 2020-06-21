d7c7ea7902cbd29a259e958050f4ca3ed62c7c84
byBlurr <dbldumont11@gmail.com>
2020-06-21 20:12:46 +0100

    Added PostTemporaryMessageAsync

--- bBotLibrary/Util.cs
+++ bBotLibrary/Util.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Discord.Net.Bot
 {
@@ -14,6 +15,16 @@ namespace Discord.Net.Bot
             if (!Directory.Exists(directory)) Directory.CreateDirectory(directory);
 
             return File.Exists(Path.Combine(directory, filename));
+        }
+
+        /// Post a message to a channel, automatically deletes after the time in milliseconds.
+        public static async Task PostTemporaryMessageAsync(ITextChannel channel, string text = null, bool isTTS = false, Embed embed = null, RequestOptions options = null, int time = 5000)
+        {
+            var message_thread = Task.Run(async () => { 
+                var msg = await channel.SendMessageAsync(text, isTTS, embed, options);
+                await Task.Delay(time);
+                await msg.DeleteAsync();
+            });
         }
     }
 }
