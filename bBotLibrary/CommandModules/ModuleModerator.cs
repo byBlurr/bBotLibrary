e921fd661209001f306eb92d4f57e755048decc5
byBlurr <dbldumont11@gmail.com>
2020-06-21 23:14:10 +0100

    Finalised ModuleModerator

--- bBotLibrary/CommandModules/ModuleModerator.cs
+++ bBotLibrary/CommandModules/ModuleModerator.cs
@@ -1,13 +1,13 @@
 ﻿using Discord.Commands;
+using Discord.WebSocket;
+using System;
 using System.Threading.Tasks;
 
 namespace Discord.Net.Bot.CommandModules
 {
-    public abstract class ModuleModerator : ModuleBase
+    public static class ModuleModerator
     {
-        [Command("kick")]
-        [Alias("k")]
-        private async Task KickAsync(IGuildUser user, [Remainder] string reason)
+        public static async Task KickAsync(ICommandContext Context, IGuildUser user, [Remainder] string reason)
         {
             string message = $"{Context.User.ToString()} kicked {user.ToString()}\n{reason}";
             EmbedBuilder embed = new EmbedBuilder()
@@ -22,10 +22,7 @@ namespace Discord.Net.Bot.CommandModules
             await Util.Logger(new LogMessage(LogSeverity.Info, "Moderation", $"{Context.User.ToString()} kicked {user.ToString()}"));
             await Task.CompletedTask;
         }
-
-        [Command("ban")]
-        [Alias("b")]
-        private async Task BanAsync(IGuildUser user, [Remainder] string reason)
+        public static async Task BanAsync(ICommandContext Context, IGuildUser user, [Remainder] string reason)
         {
             string message = $"{Context.User.ToString()} banned {user.ToString()}\n{reason}";
             EmbedBuilder embed = new EmbedBuilder()
@@ -40,10 +37,7 @@ namespace Discord.Net.Bot.CommandModules
             await Util.Logger(new LogMessage(LogSeverity.Info, "Moderation", $"{Context.User.ToString()} banned {user.ToString()}"));
             await Task.CompletedTask;
         }
-
-        [Command("warn")]
-        [Alias("w")]
-        private async Task WarnAsync(IGuildUser user, [Remainder] string reason)
+        public static async Task WarnAsync(ICommandContext Context, IGuildUser user, [Remainder] string reason)
         {
             string message = $"{Context.User.ToString()} warned {user.ToString()}\n{reason}";
             EmbedBuilder embed = new EmbedBuilder()
@@ -58,10 +52,7 @@ namespace Discord.Net.Bot.CommandModules
             await Util.Logger(new LogMessage(LogSeverity.Info, "Moderation", $"{Context.User.ToString()} warned {user.ToString()}"));
             await Task.CompletedTask;
         }
-
-        [Command("mute")]
-        [Alias("m")]
-        private async Task MuteAsync(IGuildUser user, [Remainder] string reason)
+        public static async Task MuteAsync(ICommandContext Context, IGuildUser user, [Remainder] string reason)
         {
             IRole mutedRole = null;
             foreach (IRole role in Context.Guild.Roles)
@@ -94,6 +85,65 @@ namespace Discord.Net.Bot.CommandModules
                     Color = Color.DarkRed
                 };
                 await Context.User.SendMessageAsync(null, false, embed.Build());
+            }
+
+            await Task.CompletedTask;
+        }
+        public static async Task UnmuteAsync(ICommandContext Context, IGuildUser user)
+        {
+            IRole mutedRole = null;
+            foreach (IRole role in Context.Guild.Roles)
+            {
+                if (role.Name.ToLower().Equals("[muted]")) mutedRole = role;
+            }
+
+            if (mutedRole != null)
+            {
+                string message = $"{Context.User.ToString()} unmuted {user.ToString()}";
+                EmbedBuilder embed = new EmbedBuilder()
+                {
+                    Author = new EmbedAuthorBuilder() { Name = "User Unmuted", IconUrl = Context.User.GetAvatarUrl() },
+                    Description = message,
+                    Color = Color.DarkGreen
+                };
+
+                await user.RemoveRoleAsync(mutedRole);
+                await user.SendMessageAsync(null, false, embed.Build());
+                await Util.Logger(new LogMessage(LogSeverity.Info, "Moderation", $"{Context.User.ToString()} unmuted {user.ToString()}"));
+            }
+            else
+            {
+                await Context.Message.DeleteAsync();
+                EmbedBuilder embed = new EmbedBuilder()
+                {
+                    Title = "No mute role was found",
+                    Description = "The muted role must be called `[muted]`.",
+                    Color = Color.DarkRed
+                };
+                await Context.User.SendMessageAsync(null, false, embed.Build());
+            }
+
+            await Task.CompletedTask;
+        }
+        public static async Task FilterMutedAsync(SocketMessage message)
+        {
+            if (message.Author.IsBot) return;
+
+            IRole mutedRole = null;
+            IGuild guild = (message.Channel as IGuildChannel).Guild;
+
+            foreach (IRole role in guild.Roles)
+            {
+                if (role.Name.ToLower().Equals("[muted]")) mutedRole = role;
+            }
+
+            if (mutedRole != null)
+            {
+                var roleIds = (message.Author as IGuildUser).RoleIds;
+                foreach (ulong id in roleIds)
+                {
+                    if (id == mutedRole.Id) await message.DeleteAsync();
+                }
             }
 
             await Task.CompletedTask;
