9bd6dab54a476de85f740c75a66308477f661668
byBlurr <dbldumont11@gmail.com>
2020-07-07 19:55:44 +0100

    Changes to help command, added prefix command

--- LorisAngelBot/Modules/HelpModule.cs
+++ LorisAngelBot/Modules/HelpModule.cs
@@ -14,16 +14,17 @@ namespace LorisAngelBot.Modules
 
         BotCommand[] Commands = new BotCommand[]
         {
-            new BotCommand("SHIP", "`-ship @user1 @user2`, `-ship name1 name2`, `-ship user2`, `-ship name2`", "Test how strong your relationship is!", CommandCategory.Games),
-            new BotCommand("PUNISH", "`-punish @user`", "Punish them for their actions!", CommandCategory.Games),
-            new BotCommand("REVERSE", "`-reverse <message>`", "Reverse the message!", CommandCategory.Games),
-            new BotCommand("8BALL", "`-8ball <question>`", "Ask the bot a question!", CommandCategory.Games),
-            new BotCommand("QUOTE", "`-quote @author <message>`", "Save a quote to the database! (WIP)", CommandCategory.Games),
-            new BotCommand("OLDEST", "`-oldest`", "Find out who the oldest user in the guild is!", CommandCategory.User),
-            new BotCommand("AVATAR", "`-avatar @user`", "View the users avatar!", CommandCategory.User),
-            new BotCommand("WHOIS", "`-whois @user`", "View details about the user!", CommandCategory.User),
-            new BotCommand("INVITE", "`-invite`, `inv`", "Get the invite link to invite Loris Angel to your server!", CommandCategory.BotRelated),
-            new BotCommand("BINARY", "`-binary <text>`", "Convert text to binary!", CommandCategory.Tools),
+            new BotCommand("SHIP", "`-ship @user1 @user2`, `-ship name1 name2`, `-ship user2`, `-ship name2`", "Test how strong your relationship is!", CommandCategory.Games, "Jimmy"),
+            new BotCommand("PUNISH", "`-punish @user`", "Punish them for their actions!", CommandCategory.Games, "Jimmy, Ras"),
+            new BotCommand("REVERSE", "`-reverse <message>`", "Reverse the message!", CommandCategory.Games, ""),
+            new BotCommand("8BALL", "`-8ball <question>`", "Ask the bot a question!", CommandCategory.Games, "Starri"),
+            new BotCommand("QUOTE", "`-quote @author <message>`", "Save a quote to the database! (WIP)", CommandCategory.Games, ""),
+            new BotCommand("OLDEST", "`-oldest`", "Find out who the oldest user in the guild is!", CommandCategory.User, ""),
+            new BotCommand("AVATAR", "`-avatar @user`", "View the users avatar!", CommandCategory.User, "Libby"),
+            new BotCommand("WHOIS", "`-whois @user`", "View details about the user!", CommandCategory.User, "Libby"),
+            new BotCommand("INVITE", "`-invite`, `inv`", "Get the invite link to invite Loris Angel to your server!", CommandCategory.BotRelated, ""),
+            new BotCommand("PREFIX", "`-prefix <prefix>`", "Change the prefix to the bot for your server!", CommandCategory.BotRelated, ""),
+            new BotCommand("BINARY", "`-binary <text>`", "Convert text to binary!", CommandCategory.Tools, ""),
         };
 
         [Command("invite")]
@@ -32,6 +33,20 @@ namespace LorisAngelBot.Modules
         {
             await Context.Message.DeleteAsync();
             await Context.User.SendMessageAsync($"Invite Loris Angel to your server: {Util.GetInviteLink(729696788097007717)}");
+        }
+
+        [Command("prefix")]
+        [RequireUserPermission(GuildPermission.ManageGuild)]
+        private async Task PrefixAsybc(string prefix)
+        {
+            await Context.Message.DeleteAsync();
+
+            BotConfig conf = BotConfig.Load();
+            var gconf = conf.GetConfig(Context.Guild.Id);
+            gconf.Prefix = prefix;
+            conf.Save();
+
+            await Context.Channel.SendMessageAsync($"The prefix for the bot in this server has been succefully changed to ''{prefix}''.");
         }
 
         [Command("help")]
@@ -48,6 +63,8 @@ namespace LorisAngelBot.Modules
                 if (command.Category.ToString().ToLower() == section.ToLower() || command.Handle.ToLower() == section.ToLower())
                 {
                     string help = $"{command.Handle}\nUsage:\n{command.Usage}\n{command.Description}";
+                    if (command.Request != "") help = $"{help}\nFeature requested by: {command.Request}";
+
                     CommandHelpText = CommandHelpText + "\n\n" + help;
                     SendInDm = true;
                 }
@@ -90,13 +107,15 @@ namespace LorisAngelBot.Modules
         public string Usage { get; set; }
         public string Description { get; set; }
         public CommandCategory Category { get; set; }
+        public string Request { get; set; }
 
-        public BotCommand(string handle, string usage, string description, CommandCategory category)
+        public BotCommand(string handle, string usage, string description, CommandCategory category, string request)
         {
             Handle = handle ?? throw new ArgumentNullException(nameof(handle));
             Usage = usage ?? throw new ArgumentNullException(nameof(usage));
             Description = description ?? throw new ArgumentNullException(nameof(description));
             Category = category;
+            Request = request;
         }
     }
 }
