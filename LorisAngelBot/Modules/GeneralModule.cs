3485bd3e7c1a66f9c317ba29f124d7e8b7b592a3
byBlurr <dbldumont11@gmail.com>
2020-07-08 19:07:31 +0100

    Added uptime command

--- LorisAngelBot/Modules/GeneralModule.cs
+++ LorisAngelBot/Modules/GeneralModule.cs
@@ -157,8 +157,37 @@ namespace LorisAngelBot.Modules
             {
                 Title = "Lori's Angels Statistics",
                 Color = Color.DarkPurple,
-                Description = $"Out of the {guilds} guilds I am in there are {total} total users, {users} of which are from this guild!",
-                Footer = new EmbedFooterBuilder() { Text = $"{Util.GetRandomEmoji()}  Requested by {Context.User.Username}#{Context.User.Id}"}
+                Description = $"Out of the {guilds} guilds I am watching {total} total users, {users} of which are from this guild!",
+                Footer = new EmbedFooterBuilder() { Text = $"{Util.GetRandomEmoji()}  Requested by {Context.User.Username}#{Context.User.Id}" }
+            };
+
+            await Context.Channel.SendMessageAsync(null, false, embed.Build());
+        }
+
+        [Command("uptime")]
+        private async Task UptimeAsync()
+        {
+            await Context.Message.DeleteAsync();
+
+            BotConfig conf = BotConfig.Load();
+            DateTime time = DateTime.UtcNow;
+            int minutes = (int)((time - conf.LastStartup).TotalMinutes);
+            int uptime = 0;
+            string m = "minutes";
+
+            if (minutes >= 60)
+            {
+                uptime = minutes / 60;
+                m = "hours";
+            }
+            else uptime = minutes;
+
+            EmbedBuilder embed = new EmbedBuilder()
+            {
+                Title = "Lori's Angels Statistics",
+                Color = Color.DarkPurple,
+                Description = $"Lori's Angel has been online since {time}, thats an uptime of {uptime} {m}!",
+                Footer = new EmbedFooterBuilder() { Text = $"{Util.GetRandomEmoji()}  Requested by {Context.User.Username}#{Context.User.Id}" }
             };
 
             await Context.Channel.SendMessageAsync(null, false, embed.Build());
