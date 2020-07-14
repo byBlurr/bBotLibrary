4e2e658dcc21d4ec8971f491aef78455da2c9e53
byBlurr <dbldumont11@gmail.com>
2020-07-14 22:32:45 +0100

    More towards quiz

--- LorisAngelBot/Modules/QuizModule.cs
+++ LorisAngelBot/Modules/QuizModule.cs
@@ -1,9 +1,10 @@
 ﻿using Discord;
 using Discord.Commands;
 using Discord.Net.Bot;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace LorisAngelBot.Modules
@@ -23,7 +24,68 @@ namespace LorisAngelBot.Modules
                 if (pickedTopic != QuizCategory.Unspecified)
                 {
                     // Start the quiz
+                    QuizGame quiz = new QuizGame();
+                    List<QuizQuestion> questions = QuizFile.Load().GetTopicQuestions(pickedTopic);
+                    IUserMessage message = null;
 
+                    for (int i = 0; i < questions.Count; i++)
+                    {
+                        bool endGame = false;
+                        if (!endGame)
+                        {
+                            if (message != null) await message.DeleteAsync();
+                            if (questions[i] != null)
+                            {
+                                EmbedBuilder embed = new EmbedBuilder()
+                                {
+                                    Title = $"Quiz {Util.ToUppercaseFirst(pickedTopic.ToString())} -  Question {i + 1}",
+                                    Description = $"**Q: {Util.ToUppercaseFirst(questions[i].Question)}**\n\n" +
+                                    $"**A**  {questions[i].Options[0]}\n" +
+                                    $"**B**  {questions[i].Options[1]}\n" +
+                                    $"**C**  {questions[i].Options[2]}\n" +
+                                    $"**D**  {questions[i].Options[3]}\n\n" +
+                                    $"Time: 30 seconds",
+                                    Color = Color.DarkPurple,
+                                    Footer = new EmbedFooterBuilder() { Text = $"{Util.GetRandomEmoji()}  Requested by {Context.User.Username}#{Context.User.Discriminator}" }
+                                };
+                                message = await Context.Channel.SendMessageAsync(null, false, embed.Build());
+
+
+                                IEmote[] reactions = {
+                                    new Discord.Emoji("🇦"),
+                                    new Discord.Emoji("🇧"),
+                                    new Discord.Emoji("🇨"),
+                                    new Discord.Emoji("🇩"),
+                                    new Discord.Emoji("❌"),
+                                };
+
+                                await message.AddReactionsAsync(reactions);
+                                await Task.Delay(10000);
+
+                                // See who was correct
+                                
+
+                                await message.RemoveAllReactionsAsync();
+                                embed.Description = $"Answer was **{questions[i].Options[questions[i].Answer]}**\n\nCorrect:\n**{"Not sure who got it correct because I am dumb!"}**";
+                                await message.ModifyAsync(x => x.Embed = embed.Build());
+
+                                if (endGame) i = questions.Count + 1;
+                                await Task.Delay(10000);
+                            }
+                        }
+                    }
+
+                    if (message != null) await message.DeleteAsync();
+
+                    EmbedBuilder finishedEmbed = new EmbedBuilder()
+                    {
+                        Title = "Quiz Finished",
+                        Description = "The quiz has ended!",
+                        Color = Color.DarkPurple,
+                        Footer = new EmbedFooterBuilder() { Text = $"{Util.GetRandomEmoji()}  Requested by {Context.User.Username}#{Context.User.Discriminator}" }
+                    };
+
+                    await Context.Channel.SendMessageAsync(null, false, finishedEmbed.Build());
                 }
                 else
                 {
@@ -42,6 +104,15 @@ namespace LorisAngelBot.Modules
             else
             {
                 // Already game active!
+                // Display available topics
+                EmbedBuilder embed = new EmbedBuilder()
+                {
+                    Title = "Quiz Help",
+                    Description = $"There is already a quiz in motion.",
+                    Color = Color.DarkPurple,
+                    Footer = new EmbedFooterBuilder() { Text = $"{Util.GetRandomEmoji()}  Requested by {Context.User.Username}#{Context.User.Discriminator}" }
+                };
+                await Context.Channel.SendMessageAsync(null, false, embed.Build());
             }
         }
 
@@ -71,19 +142,64 @@ namespace LorisAngelBot.Modules
     public class QuizFile
     {
 
+        [JsonIgnore]
+        static readonly string Dir = Path.Combine(AppContext.BaseDirectory, "quiz");
+
+        [JsonIgnore]
+        static readonly string Filename = "questions.json";
+
+        public List<QuizQuestion> Questions { get; set; }
+
+        public Discord.Emoji C { get; set; }
+
+        public QuizFile()
+        {
+            Questions = new List<QuizQuestion>();
+        }
+
+        public List<QuizQuestion> GetTopicQuestions(QuizCategory topic)
+        {
+            List<QuizQuestion> questions = new List<QuizQuestion>();
+            foreach (QuizQuestion q in Questions)
+            {
+                if (q.Category == topic) questions.Add(q);
+            }
+
+            return questions;
+        }
+
+        public static bool Exists()
+        {
+            if (!Directory.Exists(Dir)) Directory.CreateDirectory(Dir);
+            if (!File.Exists(Path.Combine(Dir, Filename)))
+            {
+                new QuizFile().Save();
+            }
+
+            return File.Exists(Path.Combine(Dir, Filename));
+        }
+
+        public void Save() => File.WriteAllText(Path.Combine(Dir, Filename), ToJson());
+
+        public static QuizFile Load()
+        {
+            return JsonConvert.DeserializeObject<QuizFile>(File.ReadAllText(Path.Combine(Dir, Filename)));
+        }
+
+        public string ToJson() => JsonConvert.SerializeObject(this, Formatting.Indented);
     }
 
     public class QuizQuestion
     {
         public string Question { get; set; }
-        public string Answer { get; set; }
+        public int Answer { get; set; }
         public string[] Options { get; set; }
         public QuizCategory Category { get; set; }
 
-        public QuizQuestion(string question, string answer, string[] options, QuizCategory category)
+        public QuizQuestion(string question, int answer, string[] options, QuizCategory category)
         {
             Question = Util.ToUppercaseFirst(question);
-            Answer = Util.ToUppercaseFirst(answer);
+            Answer = answer;
             Options = options;
             Category = category;
         }
