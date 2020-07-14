b587d92473920d09fe6771111f4c56e7432c56ce
byBlurr <dbldumont11@gmail.com>
2020-07-14 03:55:47 +0100

    More work towards quizzes

--- LorisAngelBot/Modules/QuizModule.cs
+++ LorisAngelBot/Modules/QuizModule.cs
@@ -1,14 +1,71 @@
-﻿using Discord.Commands;
+﻿using Discord;
+using Discord.Commands;
 using Discord.Net.Bot;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace LorisAngelBot.Modules
 {
     public class QuizModule : ModuleBase
     {
+        [Command("quiz")]
+        private async Task QuizAsync(string topic = "")
+        {
+            await Context.Message.DeleteAsync();
 
+            if (QuizGames.GetGame(Context.Guild.Id) == null)
+            {
+                QuizCategory pickedTopic = QuizCategory.Unspecified;
+                Enum.TryParse(Util.ToUppercaseFirst(topic), out pickedTopic);
+
+                if (pickedTopic != QuizCategory.Unspecified)
+                {
+                    // Start the quiz
+
+                }
+                else
+                {
+                    // Display available topics
+                    EmbedBuilder embed = new EmbedBuilder()
+                    {
+                        Title = "Quiz Help",
+                        Description = $"Choose a topic for your quiz using the following command...\n`{CommandHandler.GetPrefix(Context.Guild.Id)}quiz <topic>\n\n" +
+                        $"**Topics**\nGeneral, Sports, ExtremeSports, Anime, Gaming, Computing, Music",
+                        Color = Color.DarkPurple,
+                        Footer = new EmbedFooterBuilder() { Text = $"{Util.GetRandomEmoji()}  Requested by {Context.User.Username}#{Context.User.Discriminator}" }
+                    };
+                    await Context.Channel.SendMessageAsync(null, false, embed.Build());
+                }
+            }
+            else
+            {
+                // Already game active!
+            }
+        }
+
+    }
+
+    public class QuizGames
+    {
+        public static List<QuizGame> Games = new List<QuizGame>();
+
+        public static QuizGame GetGame(ulong guild)
+        {
+            foreach (QuizGame game in Games)
+            { 
+                if (game.Guild == guild)
+                {
+                    return game;
+                }
+            }
+            return null;
+        }
+    }
+    public class QuizGame
+    {
+        public ulong Guild { get; set; }
     }
 
     public class QuizFile
@@ -34,6 +91,6 @@ namespace LorisAngelBot.Modules
 
     public enum QuizCategory
     {
-        General, Sports, ExtremeSports, Anime, Gaming, Computing
+        Unspecified, General, Sports, ExtremeSports, Anime, Gaming, Computing, Music
     }
 }
--- bBotLibrary/Util.cs
+++ bBotLibrary/Util.cs
@@ -70,7 +70,7 @@ namespace Discord.Net.Bot
                 return string.Empty;
             }
 
-            return char.ToUpper(s[0]) + s.Substring(1);
+            return char.ToUpper(s[0]) + s.Substring(1).ToLower();
         }
 
         /// Send error message
