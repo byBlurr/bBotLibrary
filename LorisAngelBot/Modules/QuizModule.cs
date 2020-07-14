0c7ec3d1db1585a0b3228e7194e2d78cabe3ad7e
byBlurr <dbldumont11@gmail.com>
2020-07-14 02:19:33 +0100

    Start quiz module

---
+++ LorisAngelBot/Modules/QuizModule.cs
@@ -0,0 +1,39 @@
+﻿using Discord.Commands;
+using Discord.Net.Bot;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LorisAngelBot.Modules
+{
+    public class QuizModule : ModuleBase
+    {
+
+    }
+
+    public class QuizFile
+    {
+
+    }
+
+    public class QuizQuestion
+    {
+        public string Question { get; set; }
+        public string Answer { get; set; }
+        public string[] Options { get; set; }
+        public QuizCategory Category { get; set; }
+
+        public QuizQuestion(string question, string answer, string[] options, QuizCategory category)
+        {
+            Question = Util.ToUppercaseFirst(question);
+            Answer = Util.ToUppercaseFirst(answer);
+            Options = options;
+            Category = category;
+        }
+    }
+
+    public enum QuizCategory
+    {
+        General, Sports, ExtremeSports, Anime, Gaming, Computing
+    }
+}
