4d101c69535d05e519410ea40c9194506183206f
byBlurr <dbldumont11@gmail.com>
2020-07-29 23:17:37 +0100

    Removed streama and quiz stuff

--- LorisAngelBot/LorisAngel.cs
+++ LorisAngelBot/LorisAngel.cs
@@ -24,13 +24,8 @@ namespace LorisAngelBot
             commands.Add(new BotCommand("DICE", "`-dice <amount>`", "Roll a dice or 2 or 50!", CommandCategory.Games, "Mir"));
             commands.Add(new BotCommand("SNAKE", "`-snake @player2 @player3 @player4`", "Start a game of snake! Up to 4 players! [BETA]", CommandCategory.Main, "Tay", true));
             commands.Add(new BotCommand("WHO", "`-who <question>` (Mention people in the question)", "Ask a question, the bot will select a random user that is mentioned!", CommandCategory.Games, ""));
-            //commands.Add(new BotCommand("QUIZ", "`-quiz <topic>`", "Start a quiz!", CommandCategory.Games, "", true));
-            //commands.Add(new BotCommand("TIC TAC TOE", "`-ttt`", "Start a game of Tic Tac Toe!", CommandCategory.Games, "", true));
-            //commands.Add(new BotCommand("PUZZLE", "`-puzzle`", "Start a puzzle!", CommandCategory.Games, "", true));
             commands.Add(new BotCommand("TICTACTOE | NAUGHTS & CROSSES", "`-ttt`", "Start a game of Tic Tac Toe (Naughts and Crosses)", CommandCategory.Main, "Libby", true));
 
-
-            //commands.Add(new BotCommand("QUOTE", "`-quote @author <message>`", "Save a quote to the database! (WIP)", CommandCategory.Games, ""));
 
             // NSFW
             commands.Add(new BotCommand("PUNISH", "`-punish @user`", "Punish them for their actions!", CommandCategory.NSFW, "Jimmy, Ras"));
@@ -44,7 +39,6 @@ namespace LorisAngelBot
             commands.Add(new BotCommand("INVITE", "`-invite`, `inv`", "Get the invite link to invite Loris Angel to your server!", CommandCategory.BotRelated, ""));
             commands.Add(new BotCommand("REQUEST", "`-request`", "Get the invite link to the support server where you can request new features or get help!", CommandCategory.BotRelated, ""));
             commands.Add(new BotCommand("PREFIX", "`-prefix <prefix>`", "Change the prefix to the bot for your server!", CommandCategory.BotRelated, ""));
-            //commands.Add(new BotCommand("STREAM", "`-stream <stream-url>`", "Every 24 hours a random stream will be picked and displayed on the status, list of streams resets each week.", CommandCategory.BotRelated, ""));
             commands.Add(new BotCommand("UPTIME", "`-uptime`", "Check how long the bot has been online.", CommandCategory.BotRelated, ""));
 
             // Tools
@@ -64,56 +58,18 @@ namespace LorisAngelBot
         private async Task ReadyAsync()
         {
             RelationshipFile.Exists();
-            StreamFile.Exists();
-            QuizFile.Exists();
 
             await bot.SetStatusAsync(UserStatus.Online);
 
             string stream = "https://www.twitch.tv/pengu";
-            StreamFile file = StreamFile.Load();
-            if (file.Current != null) stream = file.Current.Url;
 
-            //await bot.SetActivityAsync(new Game($"{bot.Guilds.Count} servers {Util.GetRandomEmoji()}", ActivityType.CustomStatus));
             var status = Task.Run(async () => {
                 while (true)
                 {
-                    if (DateTime.UtcNow.Hour == 0 && DateTime.UtcNow.Minute <= 1)
-                    {
-                        file = StreamFile.Load();
-                        if (file.Current != null) stream = file.Current.Url;
-                    }
-
                     await bot.SetGameAsync($"to {bot.Guilds.Count} servers {Util.GetRandomHeartEmoji()}", stream, ActivityType.Streaming);
                     await Task.Delay(2500);
                 }
             });
-
-            QuizFile qfile = QuizFile.Load();
-            if (qfile.GetTopicQuestions(QuizCategory.General).Count == 0)
-            {
-                string[] options = { "Knight", "Jeroen", "Blurr", "Kruze" };
-                qfile.Questions.Add(new QuizQuestion("Who wrote this bot?", 2, options, QuizCategory.General));
-                qfile.Save();
-            }
-
-            /**
-            var streams = Task.Run(async () => {
-                while (true)
-                {
-                    if (DateTime.UtcNow.Hour == 0)
-                    {
-                        await StreamModule.PickNewWinnerAsync();
-                        if (DateTime.UtcNow.Day == 1)
-                        {
-                            StreamFile file = StreamFile.Load();
-                            file.Streams.Clear();
-                            file.Save();
-                        }
-                        await Task.Delay((60000 * 60) * 20);
-                    }
-                    await Task.Delay(60000);
-                }
-            });**/
         }
     }
 
