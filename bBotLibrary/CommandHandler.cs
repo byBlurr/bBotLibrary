acff262514c34356b018386d3c6dc7bbe25b09e2
byBlurr <dbldumont11@gmail.com>
2020-07-30 00:45:26 +0100

    Command handler runs on a seperate thread

--- bBotLibrary/CommandHandler.cs
+++ bBotLibrary/CommandHandler.cs
@@ -85,44 +85,48 @@ namespace Discord.Net.Bot
         public virtual void RegisterCommands(List<BotCommand> commands) { }
         private async Task HandleCommandAsync(SocketMessage pMsg)
         {
-            SocketUserMessage message = pMsg as SocketUserMessage;
-            if (message == null) return;
-            var context = new SocketCommandContext(bot, message);
-            if (message.Author.IsBot) return;
-
-            string prefix = "";
-            BotConfig config = BotConfig.Load();
-            if (configType == ConfigType.Solo)
-                prefix = config.SoloConfig.Prefix;
-            else
-                prefix = config.GetConfig((message.Channel as IGuildChannel).GuildId).Prefix;
-
-            if (message.Content.StartsWith(bot.CurrentUser.Mention) && message.Content.Length == bot.CurrentUser.Mention.Length)
+            var cmd = Task.Run(async () =>
             {
-                await context.Channel.SendMessageAsync($"Try `{prefix}help`");
-                return;
-            }
+                SocketUserMessage message = pMsg as SocketUserMessage;
+                if (message == null) return;
+                var context = new SocketCommandContext(bot, message);
+                if (message.Author.IsBot) return;
 
-            int argPos = 0;
-            if (message.HasStringPrefix(prefix, ref argPos) || message.HasMentionPrefix(bot.CurrentUser, ref argPos))
-            {
-                var result = await commands.ExecuteAsync(context, argPos, map);
-                if (!result.IsSuccess && result.ErrorReason != "Unknown command.")
+                string prefix = "";
+                BotConfig config = BotConfig.Load();
+                if (configType == ConfigType.Solo)
+                    prefix = config.SoloConfig.Prefix;
+                else
+                    prefix = config.GetConfig((message.Channel as IGuildChannel).GuildId).Prefix;
+
+                if (message.Content.StartsWith(bot.CurrentUser.Mention) && message.Content.Length == bot.CurrentUser.Mention.Length)
                 {
-                    if (result.ErrorReason.Contains("Collection was modified")) return;
-
-                    await Util.Logger(new LogMessage(LogSeverity.Warning, "Commands", result.ErrorReason, null));
-
-                    EmbedBuilder embed = new EmbedBuilder()
-                    {
-                        Title = "Command Error",
-                        Description = result.ErrorReason,
-                        Color = Color.DarkRed
-                    };
-
-                    await context.Channel.SendMessageAsync(null, false, embed.Build());
+                    await context.Channel.SendMessageAsync($"Try `{prefix}help`");
+                    return;
                 }
-            }
+
+                int argPos = 0;
+                if (message.HasStringPrefix(prefix, ref argPos) || message.HasMentionPrefix(bot.CurrentUser, ref argPos))
+                {
+                    var result = await commands.ExecuteAsync(context, argPos, map);
+                    if (!result.IsSuccess && result.ErrorReason != "Unknown command.")
+                    {
+                        if (result.ErrorReason.Contains("Collection was modified")) return;
+
+                        await Util.Logger(new LogMessage(LogSeverity.Warning, "Commands", result.ErrorReason, null));
+
+                        EmbedBuilder embed = new EmbedBuilder()
+                        {
+                            Title = "Command Error",
+                            Description = result.ErrorReason,
+                            Color = Color.DarkRed
+                        };
+
+                        await context.Channel.SendMessageAsync(null, false, embed.Build());
+                    }
+                }
+
+            });
         }
 
         public async Task ConfigureAsync() { await commands.AddModulesAsync(Assembly.GetEntryAssembly(), map); }
