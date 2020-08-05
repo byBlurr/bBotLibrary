c7f19f203a95ace5339147bdbdf0e6b761bcec2b
byBlurr <dbldumont11@gmail.com>
2020-08-05 07:03:28 +0100

    Fixed avatar command

--- LorisAngelBot/Modules/GeneralModule.cs
+++ LorisAngelBot/Modules/GeneralModule.cs
@@ -49,10 +49,11 @@ namespace LorisAngelBot.Modules
         [Alias("av")]
         [RequireBotPermission(ChannelPermission.ManageMessages)]
         [RequireBotPermission(ChannelPermission.SendMessages)]
-        private async Task AvatarAsync(IUser user)
+        private async Task AvatarAsync(IUser user = null)
         {
             await Context.Message.DeleteAsync();
 
+            if (user == null) user = Context.User as IUser;
             string avatar = user.GetAvatarUrl(size: 2048);
 
             EmbedBuilder embed = new EmbedBuilder()
@@ -63,6 +64,37 @@ namespace LorisAngelBot.Modules
                 Footer = new EmbedFooterBuilder() { Text = $"{Util.GetRandomEmoji()}  Requested by {Context.User.Username}#{Context.User.Discriminator}." },
             };
 
+            await Context.Channel.SendMessageAsync(null, false, embed.Build());
+        }
+
+        [Command("avatar")]
+        [Alias("av")]
+        [RequireBotPermission(ChannelPermission.ManageMessages)]
+        [RequireBotPermission(ChannelPermission.SendMessages)]
+        private async Task AvatarAsync(ulong userid)
+        {
+            await Context.Message.DeleteAsync();
+
+            IUser user = await Context.Guild.GetUserAsync(userid) as IUser;
+            if (user == null)
+            {
+                foreach (var guild in CommandHandler.GetBot().Guilds)
+                {
+                    if (await (guild as IGuild).GetUserAsync(userid) as IUser != null)
+                    {
+                        user = await (guild as IGuild).GetUserAsync(userid) as IUser;
+                    }
+                }
+            }
+
+            string avatar = user.GetAvatarUrl(size: 2048);
+            EmbedBuilder embed = new EmbedBuilder()
+            {
+                Title = $"{user.Username}#{user.Discriminator}",
+                Color = Color.DarkPurple,
+                ImageUrl = avatar,
+                Footer = new EmbedFooterBuilder() { Text = $"{Util.GetRandomEmoji()}  Requested by {Context.User.Username}#{Context.User.Discriminator}." },
+            };
             await Context.Channel.SendMessageAsync(null, false, embed.Build());
         }
 
