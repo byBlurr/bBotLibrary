6ef5e08f81ff93ee83d0e715914edc344a803853
byBlurr <dbldumont11@gmail.com>
2021-01-28 09:28:21 +0000

    Move channel utils into own class

---
+++ bBotLibrary/Utility/Channel.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discord.Net.Bot.Utility
+{
+    public class Channel
+    {
+        // Get the first message in a channel
+        public static async Task<IMessage> GetFirstMessageAsync(IMessageChannel channel) => (await channel.GetMessagesAsync(0, Direction.After, 1).FlattenAsync()).FirstOrDefault();
+
+        // Get the last message in a channel
+        public static async Task<IMessage> GetLastMessageAsync(IMessageChannel channel) => (await channel.GetMessagesAsync(1).FlattenAsync()).FirstOrDefault();
+    }
+}
